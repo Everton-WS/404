a39bf431f12237ba01e243c425269af2a4d5efc9
Patrick Nascimento <patrick.bnu@gmail.com>
2023-08-23 19:44:00 -0300

    Update init-letsencrypt.sh

--- docker/ssl/init-letsencrypt.sh
+++ docker/ssl/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
     exit 1
 fi
 
-domains=(*.404.sol.app.br)
+domains=(www.404.sol.app.br node.404.sol.app.br java.404.sol.app.br wordpress.404.sol.app.br )
 rsa_key_size=4096
 data_path="./data/certbot"
 email="everton@doctor.com" # Adding a valid address is strongly recommended
